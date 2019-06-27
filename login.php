a2dc46f37bad759df3a20e4c2775fc6d26b8ec67
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 16:15:29 +0600

    Add_user_login_data_box_and_button

--- login.php
+++ login.php
@@ -6,17 +6,30 @@
 
 <!DOCTYPE html>
 <html>
+<head>
     <title>Log-In</title>
     <link rel ="stylesheet" href ="reg.css">
-<head>
+</head>
+<body>
     <h2 class ="header">Welcome To Log-In</h2>
 
     <div class ="box">
-    <center><img src ="img_avatar3.png" alt ="Image" class ="img"></center>
-    
+        <center><img src ="img_avatar3.png" alt ="Image" class ="img"></center>
+
+        <form action ="login.php" method ="POST" align ="center">
+            <br>
+            <label>User Email</label>
+            <input name ="email" type ="email" id ="form" placeholder ="Enter your Email"></input>
+            <br>
+
+            <label>User Password</label>
+            <input name ="pass" type ="password" id ="form" placeholder ="Enter your Password"></input>
+            <br>
+
+            <input name ="login" type ="submit" id ="button" value ="Log-In"></input>
+            <a href ="#"><input name ="reg" type ="button" id ="button" value ="Registration"></input></a>
+        </form>
     </div>
-</head>
-<body>
 
 </body>
 </html>
