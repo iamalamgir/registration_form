910faba841a0f97f9b47d9c0db81fe7531a31626
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 17:17:58 +0600

    User_data_input_function_add_rigistration_file

--- reg.php
+++ reg.php
@@ -35,6 +35,67 @@ include 'config.php';
             <input name ="reg" type ="submit" id ="button" value ="Sign-Up"></input>
             <a href ="login.php"><input name ="back" type ="button" id ="button" value ="Back to Log-In"></input></a>
         </form>
+        <?php
+            if(isset($_POST['reg'])){
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $pass = $_POST['pass'];
+                $cpass = $_POST['cpass'];
+
+                if($pass == $cpass){ 
+                    $query ="SELECT*FROM user where email='$email'";
+                    $query_check= mysqli_query($con, $query);
+
+                    if($query_check){
+                        if(mysqli_num_rows($query_check) > 0){
+                            echo"
+                                <script>
+                                    alert ('Email Already In User');
+                                    window.location.href='login.php';
+                                </script>
+                            ";
+                        }else{
+                            $insertion= "insert into user values('$name', '$email', '$pass')"; 
+                            $run= mysqli_query($con, $insertion);
+                            if($run){
+                                echo"
+                                <script>
+                                    alert ('You are Successfully Registered');
+                                    window.location.href='home.php';
+                                </script>
+                            ";
+                            }else{
+                                echo"
+                                <script>
+                                    alert ('Connection Failed');
+                                    window.location.href='reg.php';
+                                </script>
+                            ";
+                                //run else
+                            }
+                        }
+                    }else{
+                        echo"
+                            <script>
+                                alert ('Database Error');
+                                window.location.href='reg.php';
+                            </script>
+                        ";
+                        //query_check
+                    }
+                }else{
+                    echo"
+                        <script>
+                            alert ('Password & Confirm-Password Does't match');
+                            window.location.href='reg.php';
+                        </script>
+                    ";
+                    //password else
+                }
+            }else{
+                //isset ar else
+            }
+        ?>
 
     </div>
 
