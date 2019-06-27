3e70a0ea120a9964b471dd0affafe3309d93b2ef
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 17:38:20 +0600

    Add_others_function_to_home_and_config_file

--- config.php
+++ config.php
@@ -8,5 +8,10 @@ $db = "test";
 $con = mysqli_connect($host, $name, $pass) or die ('Database error !.');
 mysqli_select_db($con, $db);
 
+//Below others function
+/*$connect = mysqli_connect($host, $name, $pass, $db);
+if(!$connect){
+    echo "Database Connected Error !.".$connect;
+}*/
 
 ?>
--- home.php
+++ home.php
@@ -2,6 +2,24 @@
 
 include 'config.php';
 
+//Below others function
+/*$conn = $connect;
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"]. " " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();*/
 ?>
 
 <!DOCTYPE html>
