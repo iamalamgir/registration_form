14d0cb087c887f0eb0373cbed2d247915f7ccdbc
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 16:04:29 +0600

    Add_login_profile_image

--- login.php
+++ login.php
@@ -12,7 +12,8 @@
     <h2 class ="header">Welcome To Log-In</h2>
 
     <div class ="box">
-        
+    <center><img src ="img_avatar3.png" alt ="Image" class ="img"></center>
+    
     </div>
 </head>
 <body>
