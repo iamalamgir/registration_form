39872fed7105175ee691326b5b8d3c8acf0fceb9
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 14:11:22 +0600

    Add_user_data_box

--- reg.php
+++ reg.php
@@ -16,6 +16,24 @@
     <div class ="box">
         <center><img src ="reg_icon.png" alt ="Image" class ="img"></center>
 
+        <form action ="reg.php" method ="POST" align ="center">
+            <label>User Name</label>
+            <input name ="name" type ="text" id ="form" placeholder ="Enter your Name" required></input>
+            <br>
+
+            <label>User Email</label>
+            <input name ="email" type ="email" id ="form" placeholder ="Enter your Email" required></input>
+            <br>
+
+            <label>User Password</label>
+            <input name ="pass" type ="password" id ="form" placeholder ="Enter your Password" required></input>
+            <br>
+
+            <label>Confirm Password</label>
+            <input name ="cpass" type ="password" id ="form" placeholder ="Confirm your Password" required></input>
+
+        </form>
+
     </div>
 
 </body>
