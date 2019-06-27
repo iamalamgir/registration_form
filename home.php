0a417a908b6a529c08c7707efb06779ec77407d5
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 16:57:58 +0600

    Add_user_logOut_button_to_home

--- home.php
+++ home.php
@@ -14,8 +14,14 @@
     <h2 class ="header">Welcome To Home</h2>
 
     <div class ="box">
-    <center><img src ="home_icon.png" alt ="Image" class ="img"></center>
-    
+        <center><img src ="home_icon.png" alt ="Image" class ="img"></center>
+
+        <form action ="home.php" method ="POST" align ="center">
+            <br>
+            
+            <input name ="logout" type ="submit" id ="button" value ="Log-Out"></input>
+            
+        </form>
     </div>
 </body>
 </html>
