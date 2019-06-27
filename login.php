522a658fa8347a7ae51af501a6f7a5666043e6a1
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-27 17:24:29 +0600

    User_login_function_add_login_file

--- login.php
+++ login.php
@@ -29,6 +29,44 @@ include 'config.php';
             <input name ="login" type ="submit" id ="button" value ="Log-In"></input>
             <a href ="reg.php"><input name ="reg" type ="button" id ="button" value ="Registration"></input></a>
         </form>
+        <?php
+            if(isset($_POST['login'])){
+                $email = $_POST['email'];
+                $pass = $_POST['pass'];
+
+                $query ="SELECT*FROM user where email='$email' AND pas='$pass'";
+                $check = mysqli_query($con, $query);
+
+                    if($check){
+                        if(mysqli_num_rows($check) > 0 ){
+                            echo"
+                                <script>
+                                    alert ('You are Successfully Logged In');
+                                    window.location.href='home.php';
+                                </script>
+                            ";
+                        }else{
+                            echo"
+                                <script>
+                                    alert ('Email or Password not Correct !');
+                                    window.location.href='login.php';
+                                </script>
+                            ";
+                            //email & pass check
+                        }
+                    }else{
+                        echo"
+                            <script>
+                                alert ('Database Error');
+                                window.location.href='login.php';
+                            </script>
+                        ";
+                        //query else
+                    }
+            }else{
+                //login click check
+            }
+        ?>
     </div>
 
 </body>
